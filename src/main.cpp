332d529a480441f37fca35b3b151ee98d0930843
wraydev <louis_wraydev@outlook.com>
2021-03-05 20:19:51 +0000

    tweaked sample app - working server.
    unsecure.

--- src/main.cpp
+++ src/main.cpp
@@ -113,6 +113,8 @@ public:
         if (ec)
             fail(ec, "read");
 
+        std::cout << "Received: " <<  boost::beast::buffers_to_string(buffer_.data()) << std::endl;
+
         // Echo the message
         ws_.text(ws_.got_text());
         ws_.async_write(
@@ -233,23 +235,26 @@ private:
 int main(int argc, char* argv[])
 {
     // Check command line arguments.
-    if (argc != 4)
+    if (argc != 3)
     {
         std::cerr <<
-            "Usage: websocket-server-async <address> <port> <threads>\n" <<
+            "Usage: socketcom <port> <threads>\n" <<
             "Example:\n" <<
-            "    websocket-server-async 0.0.0.0 8080 1\n";
+            "    websocket-server-async 80 1\n";
         return EXIT_FAILURE;
     }
-    auto const address = net::ip::make_address(argv[1]);
-    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-    auto const threads = std::max<int>(1, std::atoi(argv[3]));
+
+    std::cout << "Port: " << argv[1] << std::endl;
+    std::cout << "Threads: " << argv[2] << std::endl;
+
+    auto const port = static_cast<unsigned short>(std::atoi(argv[1]));
+    auto const threads = std::max<int>(1, std::atoi(argv[2]));
 
     // The io_context is required for all I/O
     net::io_context ioc{ threads };
 
     // Create and launch a listening port
-    std::make_shared<listener>(ioc, tcp::endpoint{ address, port })->run();
+    std::make_shared<listener>(ioc, tcp::endpoint{ boost::asio::ip::tcp::v4(), port })->run();
 
     // Run the I/O service on the requested number of threads
     std::vector<std::thread> v;
